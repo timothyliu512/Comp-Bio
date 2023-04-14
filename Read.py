cd913889a24ad8dbec0e18912adbfa1e97fe8223
Timothy Liu <timothy.liu512@gmail.com>
2023-04-13 18:36:34 -0700

    READ IN DATAA

--- Read.py
+++ Read.py
@@ -1,14 +1,19 @@
 import numpy as np
 import pandas as pd
-from Bio import SeqIO
+#from Bio import SeqIO
 
 
 
 # df = pd.read_csv('SRR24133807.fastq', delimiter='\t')
 
+df = pd.DataFrame(pd.read_csv('SRR24133807.fastq', sep='\t', header=None).values.reshape(-1, 4), columns=['read_id', 'seq', '+', 'qual'])
+print(df)
+'''
 df = pd.DataFrame
 
 for seqrecord in SeqIO.parse("SRR24133807.fastq", "fastq"):
     df = pd.DataFrame(seqrecord.seq)
+  ''' 
+
     # print (seqrecord.seq)
 # print(df)
